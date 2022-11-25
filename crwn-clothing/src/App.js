bdca9ac5058bc2e55811fe14385c5fd29d3bf8bf
SJ139 <90908149+SJ139@users.noreply.github.com>
2022-11-25 07:46:04 -0500

    added sign in Page

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,8 +1,9 @@
 
 import { Routes, Route,} from 'react-router';
 import Home from '../src/routes/home/home.component'
+import SignUpForm from './components/sign-up-form/sign-up-form.component';
 import Navigation from './routes/navigation/navigation.component';
-import Signin from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 
 
 const Shop = () => {
@@ -15,7 +16,7 @@ const App = () => {
       <Route path='/' element={<Navigation/>}>
         <Route index element= {<Home />} />
         <Route path='shop' element = {<Shop />} />
-        <Route path='signin' element = {<Signin />} />
+        <Route path='auth' element = {<Authentication/>} />
       </Route>
     </Routes>
   );
--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import { getAuth,
         signInWithPopup, 
         GoogleAuthProvider,
         createUserWithEmailAndPassword,
+        signInWithEmailAndPassword,
      } from 'firebase/auth';
 
 import {
@@ -61,5 +62,10 @@ const firebaseConfig = {
     if (!email || !password)return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
+  };
 
-  }
+  export const signInAuthUserWithEmailAndPassword = async(email, password) => {
+    if (!email || !password)return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
+  };
