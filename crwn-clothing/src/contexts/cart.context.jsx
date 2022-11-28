417d95899f8636ae9d2f1df924779e8ebffc3b35
SJ139 <90908149+SJ139@users.noreply.github.com>
2022-11-27 20:33:57 -0500

    shopping basket dropdown updated

--- crwn-clothing/src/contexts/cart.context.jsx
+++ crwn-clothing/src/contexts/cart.context.jsx
@@ -1,14 +1,40 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+   //find if cartItems contains product to add
+    const existingCartItem = cartItems.find(
+        (cartItem)=> cartItem.id === productToAdd.id);
+    // if found increment quantity
+    if(existingCartItem){
+        return cartItems.map(
+            (cartItem)=> cartItem.id === productToAdd.id ?
+            {...cartItem, quantity: cartItem.quantity + 1}
+            :cartItem
+        );      
+    }    
+    
+    return [...cartItems, {productToAdd, quantity:1}];
+
+    //return new array with modified cart items new cart items
+
+}
 
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
+    cartItems: [], 
+    addItemToCart: () => {}
 });
 
 export const CartProvider = ({children}) =>{
     const [isCartOpen, setIsCartOpen] = useState (false);
-    const value = {isCartOpen, setIsCartOpen};
+    const [cartItems, setCartItems] = useState([]);
+    
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+
+    }
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
