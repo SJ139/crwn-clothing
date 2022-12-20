3a47b0641233b8a77c47416ea9da3aba71ab4298
SJ139 <90908149+SJ139@users.noreply.github.com>
2022-12-20 14:31:42 -0500

    Applied styled components to navigation

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,43 +1,83 @@
-import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
-import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
-import { UserContext } from "../../contexts/user.context";
-import '../navigation/navigation.styles.scss';
-import { signOutUser } from "../../utils/firebase/firebase.utils";
-import CartIcon from "../../components/cart-icon/cart-icon.component";
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import { CartContext } from "../../contexts/cart.context";
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
+import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from './navigation.styles';
 
 const Navigation = () => {
-    const {currentUser} = useContext(UserContext);
-    const {isCartOpen} = useContext(CartContext);
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
-    return(
+  return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to='/'>
-        <CrwnLogo className="logo"/>
-        </Link>
-        <div className='nav-links-container'>
-            <Link className='nav-link' to='/shop'>
-            SHOP 
-            </Link> 
-            {currentUser ? (
-                <span className='nav-link'onClick={signOutUser}>
-                {' '}
-                SIGN OUT {' '}</span>
-                ) : (
-                  <Link className='nav-link' to = '/auth'>
-                  SIGN IN
-                </Link>
-              )}
-              <CartIcon />
-        </div>
+      <NavigationContainer>
+        <LogoContainer to='/'>
+          <CrwnLogo />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to='/shop'>SHOP</NavLink>
+
+          {currentUser ? (
+            <NavLink as='span' onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to='/auth'>SIGN IN</NavLink>
+          )}
+          <CartIcon />
+        </NavLinks>
         {isCartOpen && <CartDropdown />}
-        
-      </div>
-      <Outlet/>
+      </NavigationContainer>
+      <Outlet />
     </Fragment>
-    );
-  };
-  export default Navigation;
+  );
+};
+
+export default Navigation;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
