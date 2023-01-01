b7c82afc648834fb28d5d8106f74acbbeaeb0c67
SJ139 <90908149+SJ139@users.noreply.github.com>
2023-01-01 10:28:18 -0500

    Added Reducers to USer Contect

--- crwn-clothing/src/contexts/user.context.jsx
+++ crwn-clothing/src/contexts/user.context.jsx
@@ -1,7 +1,7 @@
 // context allows certain data to be shared across components, such as authentication 
 // details, which can be passed to other components.
 
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth  } from "../utils/firebase/firebase.utils";
 
@@ -9,12 +9,43 @@ export const UserContext = createContext({
     currentUser: null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    console.log("dispatched");
+    console.log(action);
+    const {type, payload} = action;
+
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            } 
+        default: 
+            throw new Error(`unhandeled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    // const [currentUser, setCurrentUser] = useState(null);
+    const [currentUser, dispatch] = useReducer(userReducer,INITIAL_STATE);
+    console.log(currentUser);
+
+
+    const setCurrentUser = (user) => {
+        dispatch({type:USER_ACTION_TYPES.SET_CURRENT_USER});
+            
+    }
+
     const value = {currentUser, setCurrentUser};
     
-  
-
     //checks for changes to state of authenticated user - listener function.
     //unsubscribe stops the function from listening.
     useEffect(() => {
