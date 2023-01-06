708f7b98c758b398d370922f71b79ee8a15e100d
SJ139 <90908149+SJ139@users.noreply.github.com>
2023-01-06 14:54:24 -0500

    created base components

--- modern-banking/modern-banking-app/src/App.js
+++ modern-banking/modern-banking-app/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX}${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-         Navbar
+         <Navbar/>
         </div>
       </div>
 
@@ -18,14 +18,14 @@ const App = () => {
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className= {`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          Clients
-          Footer
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <Clients/>
+          <Footer/>
         </div>
       </div>
 
