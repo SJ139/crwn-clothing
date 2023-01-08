cb9edcb897205138b1cac559b53e30c13d401f01
SJ139 <90908149+SJ139@users.noreply.github.com>
2023-01-08 18:58:10 -0500

    completedbusiness button

--- modern-banking/modern-banking-app/src/components/Business.jsx
+++ modern-banking/modern-banking-app/src/components/Business.jsx
@@ -6,11 +6,19 @@ import Button from './Button'
 const Business = () =>(
     <section id='features' className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2>
+        <h2 className={styles.heading2}>
         You do the business, <br className='sm:block hidden'/>we’ll handle the money
         </h2>
+        <p className={`${styles.paragraph} max-w-[470px] m5 mt-5`}>
+        With the right credit card, you can improve your financial life by building credit, 
+        earning rewards and saving money. 
+        </p>
+
+        <Button styles='mt-10'/>
+
 
       </div>
+
 
     </section>
   )
